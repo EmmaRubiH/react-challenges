79fb604065990e9abc7f5169545736867a161c4f
EmmaRubiH <emmahjorting89@gmail.com>
2022-12-07 11:53:16 +0000

    add new components. and did challenge

---
+++ my-app/src/components/NavBarForm.js
@@ -0,0 +1,39 @@
+import React, { Component} from "react";
+import css from "./css/NavBarForm.module.css";
+import NavbarChild from "./NavBarChild";
+
+class NavBarForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: true,
+        }
+    }
+
+    handleButtonClick = () => {
+        this.setState((prevState) => ({
+            isLoggedIn: prevState.isLoggedIn ? false: true
+        }), () => console.log(this.state.isLoggedIn))
+    }
+
+
+    render() {
+        return (
+            <div className= {css.NavBar}>
+                <h1>My Gallery</h1>
+
+                <NavbarChild
+                isLoggedIn={this.state.isLoggedIn}
+                handleClick={this.handleButtonClick}
+                />
+            </div>
+
+        
+        )
+    }
+}
+
+
+
+export default NavBarForm
