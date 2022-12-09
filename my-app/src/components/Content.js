8b7c45632fcfba851811a7ca5e7c4da478e5e587
EmmaRubiH <emmahjorting89@gmail.com>
2022-12-09 09:58:23 +0000

    add new component, work with challenge to add a loader

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,10 +2,24 @@ import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import {savedPosts} from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
+
 
 export class Content extends Component {
     constructor(props) {
         super(props)
+
+        this.state = {
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true
+            })
+        }, 2000)
     }
 
     render() {
@@ -15,9 +29,12 @@ export class Content extends Component {
                     <h1>My Photos</h1>
                 </div>
                 <div className={css.SearchResults}>
+                    {
+                        this.state.isLoaded ?
+                        <PostItem savedPosts={savedPosts} />
+                        : <Loader />
 
-                    <PostItem savedPosts={savedPosts} />
-
+                    }
                 </div>
             </div>
         )
