0472058529baf682a6538c6a7cfa0431528fbcd8
EmmaRubiH <emmahjorting89@gmail.com>
2022-12-09 13:38:54 +0000

    working with searchbar and event handler for challenges

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -10,23 +10,45 @@ export class Content extends Component {
         super(props)
 
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: [],
         }
     }
 
     componentDidMount() {
         setTimeout(() => {
             this.setState({
-                isLoaded: true
+                isLoaded: true,
+                posts: savedPosts,
             })
         }, 2000)
+    }
+    handleChange = (e) => {
+        const name = e.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post)=>{
+            return post.name.toLowerCase().includes(name);
+        })
+        this.setState({
+            posts: filteredPosts
+        })
     }
 
     render() {
         return (
             <div className={css.Content}>
                 <div className={css.TitleBar}>
-                    <h1>My Photos</h1>
+                <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor="searchInput"></label>
+                        <input 
+                        type="search" 
+                        id="searchInput"
+                        placeholder="By Author"
+                        onChange={(e) => this.handleChange(e)}
+                        />
+                        <h4>post found {this.state.posts.length}</h4>
+                    </form>
+                    
                 </div>
                 <div className={css.SearchResults}>
                     {
